00f609553140c3dd77294d2d790bcae684c6517a
Philip J. Fredholm <philip.j.fredholm@gmail.com>
2022-10-24 12:40:02 +0200

    Fixed a  bug with the parser

--- PhilipCode/src/dataReader.cpp
+++ PhilipCode/src/dataReader.cpp
@@ -23,7 +23,6 @@ readData::readData(const std::string filename, const int skipLines, const int nC
     }
     
     //Separates the columns by ;
-    int startLoc = 0;
     int endLoc;
     std::string tempStorage;
     std::vector<std::vector<std::string>> columnStorage;
@@ -33,26 +32,48 @@ readData::readData(const std::string filename, const int skipLines, const int nC
         std::vector<std::string> newVec;
         columnStorage.push_back(newVec);
     }
-    std::cout << lineStorage.size() << std::endl;
-    std::cout << columnStorage.size() << std::endl;
+
     for (std::string line : lineStorage) {
         for (int j = 0; j < nCols ; j++) {
-            //std::cout << j << std::endl;
-            try{endLoc = line.find(";");
-            tempStorage = line.substr(startLoc, endLoc);} catch(...){std::cout << line << std::endl;}
-            columnStorage[j].push_back(tempStorage);
-
-
-            try {line = line.substr(endLoc+1);} catch(...) {continue;}
-            //std::cout << line << std::endl;
-            startLoc = endLoc;
+            endLoc = line.find(";"); //Finds the next semicolon
+            tempStorage = line.substr(0, endLoc); //Stores the things before it in a new variable.
+            columnStorage[j].push_back(tempStorage); //Stores it to the relevant vector
+            line = line.substr(endLoc+1); //Cuts off what was stored and the semicolon.
         }
     }
  
-    auto column = columnStorage[1];
-    std::cout << std::endl;
-    std::cout << column[0] << std::endl;
-    this->_mainVector = columnStorage;
+
+    //We now what to separate year, month and time into a column each.
+    //Let's make a vector with 5 columns.
+    std::vector<std::vector<std::string>> detailtedData;
+    for (int n = 0; n < nCols+2; n++) {
+        std::vector<std::string> tempVector;
+        detailtedData.push_back(tempVector);
+    }
+
+    for (std::string yearLine : columnStorage[0]) {
+        for (int j = 0; j < 3; j++) {
+            endLoc = yearLine.find("-");
+            tempStorage = yearLine.substr(0, endLoc);
+            detailtedData[j].push_back(tempStorage);
+            yearLine = yearLine.substr(endLoc+1);
+        }
+    }
+    
+
+    //Add the remaining columns from columnStorage.
+    for (int n = 0; n < nCols-1; n++) {
+        detailtedData[n+3] = columnStorage[n+1];
+    }
+
+
+
+    //
+    
+    
+    auto column = detailtedData[4];
+    std::cout << column[32] << std::endl;
+    this->_mainVector = detailtedData;
     dataFile.close();
 
 
